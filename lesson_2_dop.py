8e7d6043a20929f15b5614c24611783e183acc28
alexwlad3885 <alexwlad3885@yandex.ru>
2024-05-15 21:54:57 +0300

    commit_2

--- lesson_2_dop.py
+++ lesson_2_dop.py
@@ -6,24 +6,19 @@ if (first_window >= 3) and (first_window <= 20):
     print("В первом окне число: ", first_window)
     #   устанавливаем началные значения переменных
     #   первое число в паре
-    second_window_1 = 0
+    second_window_1 = 1
     #   второе число в паре
-    second_window_2 = 0
+    second_window_2 = 1
     #   создаем пустую строку
     result = ''
 
     for i in range(1, 21):
         second_window_1 = i
-        # исключаем 0 и отрицательные числа
-        if first_window - second_window_1 > 0:
-            second_window_2 = first_window - second_window_1
-            # исключаем повтор элементов
-            if second_window_1 < second_window_2:
-                result = result + str(second_window_1) + str(second_window_2)
-            else:
-                break
-        else:
-            break
+        for j in range(1, 21):
+            second_window_2 = j
+            if first_window % (second_window_1 + second_window_2) == 0:
+                if second_window_1 < second_window_2:
+                    result = result + str(second_window_1) + str(second_window_2)
 
     print("Во втором окне надо ввести пароль: ", result)
 
