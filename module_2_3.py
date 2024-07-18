5b2207c43f116ecdb55ff45f87f0ced4e0390f1b
alexwlad3885 <168671509+alexwlad3885@users.noreply.github.com>
2024-07-18 21:52:48 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+"""Домашняя работа по уроку 'Стиль кода часть II. Цикл While.'"""
+
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+i = 0
+while i < len(my_list):
+    if my_list[i] > 0:
+        print(f'{my_list[i]}')
+        i += 1
+    elif my_list[i] == 0:
+        i += 1
+        continue
+    else:
+        break
